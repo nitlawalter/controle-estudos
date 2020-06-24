f58a3dda4dc7e762d2786897d1b03534a58fc2e2
walter lima <nitlawalter69@gmail.com>
2020-06-24 02:27:56 -0300

    configuracao heroku

--- src/app/services/host.api.ts
+++ src/app/services/host.api.ts
@@ -1,1 +1,2 @@
-export const HOST_API: string = 'http://localhost:8080';
+//export const HOST_API: string = 'http://localhost:8080';
+export const HOST_API: string = 'https://controle-estudos-api.herokuapp.com';
